f8f987b4c384ecc00e133ab84a5c61d4b274972c
Binoosh <wbinoosh@gmail.com>
2024-04-24 11:19:45 +0530

    Setting up the react router dom for navigation, and creation of sample homepage

--- nasa-application/src/App.js
+++ nasa-application/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router,Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>test</h1>
+      <Router>
+      <Link to='/home'>Take me home country roads</Link>
+        <Routes>
+          <Route path='/home' element={<Home/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
---
+++ nasa-application/src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='bg-green-200 h-screen w-[100%]'>
+      
+    </div>
+  )
+}
