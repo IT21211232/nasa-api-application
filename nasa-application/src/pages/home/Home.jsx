f5f9b13e0c70ecfcc72752b5fbcafed33f36411f
Binoosh <wbinoosh@gmail.com>
2024-04-24 17:00:22 +0530

    modifications done to homepage

--- nasa-application/src/pages/home/Home.jsx
+++ nasa-application/src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='bg-green-200 h-screen w-[100%]'>
-      
+    <div className='bg-green-200 h-screen w-full'>
+      <div className="size-20 bg-black"></div>
     </div>
   )
 }
