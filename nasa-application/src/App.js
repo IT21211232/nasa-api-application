c508f22db82df20b0b12096c8b231c632ea84015
Binoosh <wbinoosh@gmail.com>
2024-04-24 14:06:19 +0530

    changes done to home.js file

--- nasa-application/src/App.js
+++ nasa-application/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import {BrowserRouter as Router,Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Link to='/home'>Take me home country roads</Link>
         <Routes>
           <Route path='/home' element={<Home/>}></Route>
         </Routes>
