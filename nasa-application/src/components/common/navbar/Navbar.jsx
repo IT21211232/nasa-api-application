59e19a55775c52299865286a8de11a48fce739de
Binoosh <wbinoosh@gmail.com>
2024-04-25 19:45:29 +0530

    home page animations

--- nasa-application/src/components/common/navbar/Navbar.jsx
+++ nasa-application/src/components/common/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar({navbarOptions, metaData}) {
   return (
     <nav 
     className={`w-full h-14 ${!background && 'bg-transparent'} fixed top-0 left-0 z-10 flex justify-end`}>
-      <img src={NasaLogo} alt="logo image" className={`absolute ${!stayDown ? 'h-[28px] top-2/4' : 'h-[90px] top-[calc(50vh-100px)]'} left-2/4 -translate-x-1/2 -translate-y-1/2 filter invert transition-all duration-1000`}/>
+      <img  src={NasaLogo} alt="logo image" className={`absolute  left-2/4 -translate-x-1/2 -translate-y-1/2 filter invert transition-all duration-[2000ms] ${!stayDown ? 'h-[28px] top-2/4' : 'h-[90px] max-sm:h-[70px] top-[calc(50vh-100px)]'}`}/>
       <div className="h-full w-fit px-3 flex items-center">
         {
         navbarOptions.map((data)=> (
--- nasa-application/src/pages/home/Home.jsx
+++ nasa-application/src/pages/home/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/common/navbar/Navbar'
 
-import backDropStars from '../../assets/images/backdropImageComp.png'
+import backDropStars from '../../assets/images/starrySky.svg'
+import EarthImage from '../../assets/images/earthEdit.png'
 
 export default function Home() {
   const [loaded, setLoaded] = useState(false)
@@ -23,8 +24,8 @@ export default function Home() {
   }
 
   useEffect(()=> {
-    let timer1;
-    timer1 = setTimeout(() => {
+    let loadTimer1;
+    loadTimer1 = setTimeout(() => {
       setStayDown(false)
       setLoaded(true)
     }, 1000);
@@ -32,13 +33,14 @@ export default function Home() {
   return (
     <div className="h-auto w-full min-h-screen bg-transparent">
       <div className="bg-black h-screen w-full fixed top-0 left-0 z-0">
-        <img src={backDropStars} alt="" className={`home_page_backdrop opacity-0 absolute w-full h-[120vh] object-cover transition-all duration-1000`}/>
+        <img src={backDropStars} alt="" className={`absolute opacity-80 w-full h-[160vh] object-cover ${loaded ? '-translate-y-[60vh]' : 'translate-y-0'} transition-transform duration-[2000ms] z-2`}/>
+        <img src={EarthImage} alt="" className={`absolute top-[40%] left-[50%] w-[80%] -translate-x-[50%] max-sm:w-[120vw] max-sm:scale-[1.3] ${loaded ? 'top-[30%] max-sm:top-[40%]' : 'top-[60%]' } duration-[2000ms]`}/>
       </div>
       <Navbar navbarOptions = {navbarOptions} metaData={navbarData}/>
       <div 
       onClick={()=> {setLoaded(!loaded)}}
-      className="h-screen w-full relative bg-transparent"></div>
-      <div className="h-screen w-full relative bg-black"></div>
+      className="h-[80vh] w-full relative bg-transparent max-sm:h-[70vh]"></div>
+      <div className="h-screen w-full relative bg-[#141414]"></div>
     </div>
   )
 }
