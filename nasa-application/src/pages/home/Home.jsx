c472c2d3915787313e32bc98d71c844807c9542b
Binoosh <wbinoosh@gmail.com>
2024-04-25 12:43:05 +0530

    First commit of the navbar

--- nasa-application/src/pages/home/Home.jsx
+++ nasa-application/src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/common/navbar/Navbar'
 
 export default function Home() {
   return (
     <div className="h-auto w-full min-h-screen bg-transparent">
       <div className="bg-black h-screen w-full fixed top-0 left-0 z-0"></div>
+      <Navbar/>
+      <div className="h-screen w-full relative bg-green-300"></div>
+      <div className="h-screen w-full relative bg-blue-300"></div>
     </div>
   )
 }
