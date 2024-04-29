e92765cc34f30a01e9ce7b2300c0c28732eadaaa
Binoosh <wbinoosh@gmail.com>
2024-04-29 18:58:24 +0530

    Addition of context provider to manage user session

--- nasa-application/src/App.js
+++ nasa-application/src/App.js
@@ -5,19 +5,22 @@ import Apod from './pages/apod/Apod';
 import RoverPics from './pages/roverPics/RoverPics';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import LoginRegisterContextProvider from './context/LoginRegisterContext';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Login/>}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='/home' element={<Home/>}></Route>
-          <Route path='/apod' element={<Apod/>}></Route>
-          <Route path='/rover' element={<RoverPics/>}></Route>
-        </Routes>
-      </Router>
+      <LoginRegisterContextProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login/>}></Route>
+            <Route path='/signup' element={<Signup/>}></Route>
+            <Route path='/home' element={<Home/>}></Route>
+            <Route path='/apod' element={<Apod/>}></Route>
+            <Route path='/rover' element={<RoverPics/>}></Route>
+          </Routes>
+        </Router>
+      </LoginRegisterContextProvider>
     </div>
   );
 }
