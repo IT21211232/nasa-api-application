d6cdcf8329004f28197efe57c9442db08abc78dd
Binoosh <wbinoosh@gmail.com>
2024-04-29 19:25:44 +0530

    Login functionality and session data storage created

--- nasa-application/src/context/LoginRegisterContext.jsx
+++ nasa-application/src/context/LoginRegisterContext.jsx
@@ -5,9 +5,18 @@ export default function LoginRegisterContextProvider(props) {
     const [currentUserId, setCurrentUserId] = useState(null);
     const [currentUsername, setCurrentUsername] = useState(null);
 
+    console.log(currentUserId);
+    console.log(currentUsername);
+
+    const setUserData = (id, username) => {
+      setCurrentUserId(id)
+      setCurrentUsername(username)
+    }
+
     const loginRegisterContextExports = {
         currentUsername,
         setCurrentUsername,
+        setUserData
     }
   return (
     <LoginRegisterContext.Provider value={loginRegisterContextExports}>
